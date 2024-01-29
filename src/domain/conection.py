0c7378d47c0cd8c2a4ed9aa37ba61740647c7ebc
Duuendy <endydev@gmail.com>
2024-01-29 16:33:11 -0300

    Second Commit - Instaled packages and starte conncection with db

---
+++ src/domain/conection.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Session
+
+DATABASE_URL = 'psotgresql://postgres:123456@localhost:5432/fastapi_estudos'
+DB_Engine = create_engine(DATABASE_URL, echo=True)
+DB_Session_Pool = sessionmaker(autocommit=False, autoflush=False, bind=DB_Engine)
+
+class DBModel(DeclarativeBase):
+
+    pass
+
+def get_db_connection() -> Session:
+
+    db_connection = DB_Session_Pool()
+
+    try:
+        yield db_connection
+
+    finally:
+        db_connection.close()
