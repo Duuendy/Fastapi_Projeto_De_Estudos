550ebc2ec911e7972daf54c909280782ea003232
Duuendy <endydev@gmail.com>
2024-01-31 23:46:54 -0300

    Create router GET and POST

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ from fastapi import FastAPI, Depends
 
 from sqlalchemy import text
 from sqlalchemy.orm import Session
+from src.api.router import contas_pagar_receber
 from src.domain.conection import get_db_connection
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root(db_connection: Session = Depends(get_db_connection)):
-    db_connection.execute(text("create table users (FirstName varchar(255), LastName varchar(255))"))
-    db_connection.commit()
 
-    return "Hello Word"
+
+@app.get("/")
+def root():
+    return
+
+app.include_router(contas_pagar_receber.router)
+
+#async def root(db_connection: Session = Depends(get_db_connection)):
+#     db_connection.execute(text("create table users (FirstName varchar(255), LastName varchar(255))"))
+#     db_connection.commit()
---
+++ src/api/router/contas_pagar_receber.py
@@ -0,0 +1,45 @@
+from decimal import Decimal
+from typing import List
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+router = APIRouter(prefix="/contas-a-pagar-e-receber")
+
+class contasPagarReceberResponse(BaseModel):
+    id: int
+    descricao: str
+    valor: Decimal
+    tipo: str
+
+class contasPagarReceberRequest(BaseModel):
+    descricao: str
+    valor: Decimal
+    tipo: str
+
+
+@router.get("/", response_model=List[contasPagarReceberResponse])
+def listar_contas():
+    # metodo irá retornar uma lista de informações de operações bancárias
+    return [
+        contasPagarReceberResponse(
+            id=1,
+            descricao="Aluguel",
+            valor=1000.50, 
+            tipo="PAGAR"
+        ),
+        contasPagarReceberResponse(
+            id=2,
+            descricao="Salario",
+            valor=5555.55, 
+            tipo="RECEBER"
+        ),       
+    ]    
+
+@router.post("/", response_model=contasPagarReceberResponse, status_code=201)
+def criar_conta(conta: contasPagarReceberRequest):
+    return contasPagarReceberResponse(
+            id=3,
+            descricao=conta.descricao,
+            valor=conta.valor, 
+            tipo=conta.tipo
+    )
