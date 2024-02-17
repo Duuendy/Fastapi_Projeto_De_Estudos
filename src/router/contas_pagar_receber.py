8ef2ee8dd660725997b5282d421d4101c143e610
Duuendy <endydev@gmail.com>
2024-02-17 00:52:34 -0300

    Arrumando os teste que eestavam todos quebrados / fazendo melhorias_parte 2

--- src/router/contas_pagar_receber.py
+++ src/router/contas_pagar_receber.py
@@ -15,7 +15,7 @@ router = APIRouter(prefix="/contas-a-pagar-e-receber")
 class ContasPagarReceberResponse(BaseModel):
     id: int
     descricao: str
-    valor: Decimal
+    valor: float
     tipo: str
 
     class ConfigDict():
@@ -28,7 +28,7 @@ class ContasPagarReceberResponse(BaseModel):
 
 class ContasPagarReceberRequest(BaseModel):
     descricao: str
-    valor: Decimal
+    valor: float
     tipo: str
 '''
     OBS - O Fiel, no atual momento está sendo removido de versões do Pydantic, a solução foi usar o 'json_schema_extra', mas não obtive sucesso;
@@ -40,54 +40,45 @@ class ContasPagarReceberRequest(BaseModel):
 
 
 @router.get("", response_model=List[ContasPagarReceberResponse])
-#def listar_contas(db_connection: Session = Depends(get_db_connection)) -> ContasPagarReceberResponse:
-def listar_contas():
-    # metodo irá retornar uma lista de informações de operações bancárias
-    return [
-        ContasPagarReceberResponse(
-            id=1,
-            descricao="Aluguel",
-            valor=1000.50, 
-            tipo="PAGAR"
-        ),
-        ContasPagarReceberResponse(
-            id=2,
-            descricao="Salario",
-            valor=5555.55, 
-            tipo="RECEBER"
-        ),       
-    ]    
-    # return db_connection.query(ContasPagarReceber).all()
+def listar_contas(db_connection: Session = Depends(get_db_connection)) -> ContasPagarReceberResponse:
+    return db_connection.query(ContasPagarReceber).all()
 
+# def listar_contas():
+#     # metodo irá retornar uma lista de informações de operações bancárias
+#     return [
+#         ContasPagarReceberResponse(
+#             id=1,
+#             descricao="Aluguel",
+#             valor=1000.50, 
+#             tipo="PAGAR"
+#         ),
+#         ContasPagarReceberResponse(
+#             id=2,
+#             descricao="Salario",
+#             valor=5555.55, 
+#             tipo="RECEBER"
+#         ),       
+#     ]    
+    
 @router.post("", response_model=ContasPagarReceberResponse, status_code=201)
-def criar_conta(conta: ContasPagarReceberRequest):
-    return ContasPagarReceberResponse(
-        id=3,
-        descricao=conta.descricao,
-        valor=conta.valor,
-        tipo=conta.tipo
+def criar_conta(conta_a_pagar_e_receber_request: ContasPagarReceberRequest, db_connection: Session = Depends(get_db_connection)) -> ContasPagarReceberResponse:
+    
+    #Para escrever no banco, crio uma variável que tem como modelo a Class do db_models, usando os parametros e passando em uma lista;  
+    conta_a_pagar_e_receber = ContasPagarReceber(
+        **conta_a_pagar_e_receber_request.model_dump()
     )
 
+    db_connection.add(conta_a_pagar_e_receber)
+    db_connection.commit()
+    db_connection.refresh(conta_a_pagar_e_receber)
 
-# def criar_conta(conta_a_pagar_e_receber_request: ContasPagarReceberRequest, db_connection: Session = Depends(get_db_connection)) -> ContasPagarReceberResponse:
-    
-#     #Para escrever no banco, crio uma variável que tem como modelo a Class do db_models, usando os parametros e passando em uma lista;  
-#     conta_a_pagar_e_receber = ContasPagarReceber(
-#         **conta_a_pagar_e_receber_request.model_dump()
-#     )
+    return conta_a_pagar_e_receber
 
-#     db_connection.add(conta_a_pagar_e_receber)
-#     db_connection.commit()
-#     db_connection.refresh(conta_a_pagar_e_receber)
-
-#     return conta_a_pagar_e_receber
-
-# @router.put("/{id_da_conta_pagar_receber}", response_model=ContasPagarReceberResponse, status_code=200)
-# def criar_conta(id_da_conta_pagar_receber: int,
-#                 conta_a_pagar_e_receber_request: ContasPagarReceberRequest, 
+# @router.put("/{id_conta_a_pagar_e_receber}", response_model=ContasPagarReceberResponse, status_code=200)
+# def criar_conta(conta_a_pagar_e_receber_request: ContasPagarReceberRequest, 
 #                 db_connection: Session = Depends(get_db_connection)) -> ContasPagarReceberResponse:
     
-#     conta_a_pagar_e_receber: ContasPagarReceber = db_connection.query(ContasPagarReceber).get(id_da_conta_pagar_receber)
+#     conta_a_pagar_e_receber: ContasPagarReceber = db_connection.query(ContasPagarReceber).get(id_conta_a_pagar_e_receber)
 #     conta_a_pagar_e_receber.tipo = conta_a_pagar_e_receber_request.tipo
 #     conta_a_pagar_e_receber.valor = conta_a_pagar_e_receber_request.valor
 #     conta_a_pagar_e_receber.descricao = conta_a_pagar_e_receber_request.descricao
--- test/routers/test_contas_pagar_receber_router.py
+++ test/routers/test_contas_pagar_receber_router.py
@@ -7,10 +7,11 @@ from main import app
 from fastapi.testclient import TestClient
 
 from sqlalchemy import Engine, create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
 from src.domain.conection import get_db_connection
 from src.models.db_models import ContasPagarReceber
+
 
 
 # fake = Faker()
@@ -26,98 +27,97 @@ from src.models.db_models import ContasPagarReceber
 
 #     return mocked_constas_pg
 
-#FIXTURE - Definie a função setup_database como uma função que será automaticamente executada para todos os teste
-@pytest.fixture(scope="session", autouse=True)
-def setup_database()-> Engine:
-    #URL do banco de dados
-    SQLALCHEMY_DATABASE_URL = "sqlite:///.test.db"
 
-    #Mecanismo que irá conectar ao DataBase  através da URL informada
-    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False})
+#URL do banco de dados
+SQLALCHEMY_DATABASE_URL = "sqlite:///.test.db"
 
-    #Variável responsável por criar sessões de banco de dados
-    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#Mecanismo que irá conectar ao DataBase  através da URL informada
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False})
+
+#Variável responsável por criar sessões de banco de dados
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-    def override_get_db():
-        #Instancia da sessão database
-        db = TestingSessionLocal
-        try:
-            yield db
-        finally:
-            db.close_all()
+def override_get_db() -> Session: # type: ignore
+    #Instancia da sessão database
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-    #Passo a funcao original como parametro para a funcao de teste
-    app.dependency_overrides[get_db_connection] = override_get_db
+#Passo a funcao original como parametro para a funcao de teste
+app.dependency_overrides[get_db_connection] = override_get_db
+
+    
+client = TestClient(app)
+    
+
+def test_listar_contas_pagar_receber():
+
+    ContasPagarReceber.metadata.drop_all(bind=engine)
+    ContasPagarReceber.metadata.create_all(bind=engine)
+    
+    # client.post("/contas-a-pagar-e-receber", json={'descricao': 'Aluguel', 'valor': 1000.5, 'tipo': 'PAGAR'})
+    # client.post("/contas-a-pagar-e-receber", json={'descricao': 'Salario', 'valor': 5555.55, 'tipo': 'RECEBER'})
+
+    nova_conta = {
+        "descricao": "Aluguel",
+        "valor": 1000.5,
+        "tipo": "PAGAR"
+    }
+
+    nova_conta_copy = nova_conta.copy()
+    nova_conta_copy["id"] = 1
+    #nova_conta_copy["valor"] = str(nova_conta_copy['valor'])
+
+    response = client.post("/contas-a-pagar-e-receber", json=nova_conta)
+    assert response.status_code == 201
+    assert response.json() == nova_conta_copy
+
+
+    response = client.get('/contas-a-pagar-e-receber')
+    assert response.status_code == 200
+    
+    assert response.json() == [
+        {'id': 1, 'descricao': 'Aluguel', 'valor': 1000.5, 'tipo': 'PAGAR'}, 
+        #{'id': 2, 'descricao': 'Salario', 'valor': 5555.55, 'tipo': 'RECEBER'}
+    ]
+
+def test_criar_conta_pagar_receber():
 
     ContasPagarReceber.metadata.drop_all(bind=engine)
     ContasPagarReceber.metadata.create_all(bind=engine)
 
-
-client = TestClient(app)
-
-
-def test_listar_contas_pagar_receber():
-
-    response = client.get('/contas-a-pagar-e-receber')
-    assert response.status_code == 200
-    assert response.json() == [
-        {'id': 1, 'descricao': 'Aluguel', 'valor': '1000.5', 'tipo': 'PAGAR'}, 
-        {'id': 2, 'descricao': 'Salario', 'valor': '5555.55', 'tipo': 'RECEBER'}
-    ]
-
-def test_criar_conta_pagar_receber():
     nova_conta = {
         "descricao": "Faculdade",
         "valor": 150.00,
         "tipo": "PAGAR"
     }
+
+    nova_conta_copy = nova_conta.copy()
+    nova_conta_copy["id"] = 1
+    #nova_conta_copy["valor"] = str(nova_conta_copy['valor'])
+
     response = client.post("/contas-a-pagar-e-receber", json=nova_conta)
     assert response.status_code == 201
+    assert response.json() == nova_conta_copy
     
-    
-# def teste_retornar_erro_descricao():
-#     response = client.post('/contas-a-pagar-e-receber', json={
-#         "descricao": "0123456789012345678901234567890",
-#         "valor": 150.00,
-#         "tipo": "PAGAR"
-#     })
-    
-#     assert response.status_code == 422
-    #SQLite não força o tamanho de um VARCHAR
-
-    #nova_conta_copy = nova_conta.copy()
-    #nova_conta_copy["id"] = 3
-    #assert response.json() == nova_conta_copy
-
-     
-    #Esta ocorrendo um erro onde o pytest informa que a representação numerica dos valores entre response.json() e nova_conta_copy
-    #Para a solução, testei força transformar os valores em string, porem sem sucesso 
-    #assert str(response.json()["valor"]) == str(nova_conta_copy("valor"))
-    
-    #Para a solução, testei força formatar os valores, porem sem sucesso 
-    #assert "{:.2f}".format(response.json()["valor"]) == "{:.2f}".format(nova_conta_copy["valor"])
 
 # def test_atualizar_conta_pagar_receber():
 
-#     ContasPagarReceber.metadata.drop_all(bind=engine)
-#     ContasPagarReceber.metadata.create_all(bind=engine)
+#     response = client.post("/contas-a-pagar-e-receber", json={
+#             "descricao": "Investimento",
+#             "valor": 1500,
+#             "tipo": "RECEBER"
+#         })
     
+#     id_conta_a_pagar_e_receber = response.json()["id"]
     
-#     response = client.post('/contas-a-pagar-e-receber', json=nova_conta)
-#     nova_conta = {
-#         "descricao": "Faculdade",
-#         "valor": 150.00,
-#         "tipo": "PAGAR"
-#     }
-
-#     id_da_conta_pagar_receber = response.json()['id']
-
-#     response_put = client.put('/contas-a-pagar-e-receber/{id_da_conta_pagar_receber}', json={
-#         "descricao": "Faculdade",
-#         "valor": 150.00,
-#         "tipo": "PAGAR"
-#     })
+#     response_put = client.put("/contas-a-pagar-e-receber/{id_conta_a_pagar_e_receber}", json={
+#             "descricao": "Investimento",
+#             "valor": 3000,
+#             "tipo": "RECEBER"
+#         })
 
 #     assert response_put.status_code == 200
-    
